a74cc63741ac3f8fb6e4719b45b959503370b727
gitmurat <murat.kurt@windowslive.com>
2022-06-26 15:15:58 +0300

    Add project files.

---
+++ NGCOREAPP/Operation/GenerateLink.cs
@@ -0,0 +1,18 @@
+﻿using NGCOREAPP.Tools;
+
+namespace NGCOREAPP.Operation
+{
+    public class GenerateLink
+    {
+        public string Generate()
+        {
+            string url = "";
+
+            Random random = new Random();
+
+            url += random.LowerCase()+ random.UpperCase() + random.Next(999)+ random.LowerCase() + random.UpperCase();
+
+            return url;
+        }
+    }
+}
---
+++ NGCOREAPP/Tools/ExtationsMethods.cs
@@ -0,0 +1,14 @@
+﻿namespace NGCOREAPP.Tools
+{
+    public static class ExtationsMethods
+    {
+        public static string LowerCase(this Random random)
+        {
+            return ((char)random.Next('a', 'z')).ToString();
+        }
+        public static string UpperCase(this Random random)
+        {
+            return ((char)random.Next('A', 'Z')).ToString();
+        }
+    }
+}
